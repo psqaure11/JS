e98cef782d4a410e7fb5ca07967842182b9aa9b1
Pragya Pant <pragyapant73@gmail.com>
2023-10-13 19:20:24 +0000

    done

---
+++ .devcontainer/Practice/2.js
@@ -0,0 +1,10 @@
+/*     DataTypes and ECMA standards      */
+"use strict";
+
+//number
+//string => " "
+//boolean => true/false
+//null => it's a standalone value
+//undefined => Value is not assigned
+// objects
+//symbols
