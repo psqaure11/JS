0567ef08b6d4e3bd90d854d887003b64c77e8433
Pragya Pant <pragyapant73@gmail.com>
2023-10-13 19:52:31 +0000

    done

---
+++ .devcontainer/Practice/3.js
@@ -0,0 +1,23 @@
+/*     Datatype conversion                  */
+
+let score = 33
+
+console.log(typeof(score))
+
+let score1 = "33"
+
+console.log(typeof(score1))
+
+let convert_to_num = Number(score1)
+
+console.log(typeof(convert_to_num))
+
+let bool = 0
+
+
+
+let convert_to_bool = Boolean(bool)
+
+console.log(typeof(convert_to_bool))
+console.log(bool)
+console.log(convert_to_bool)
