fd6b579170d3f560f466776b7509b504f2fdbf72
Pragya Pant <pragyapant73@gmail.com>
2023-10-14 10:50:45 +0530

    Create 4.js
    done

---
+++ .devcontainer/Practice/4.js
@@ -0,0 +1,10 @@
+/* 
+Double equals (==) is often referred to as 'loose equality' because it performs type coercion before making any comparison.
+This means that if the datatypes of the operands we are comparing are different, then the JavaScript Engine automatically
+converts one of the operands to be the same as the other one in order to make the comparison possible.
+*/
+
+/*
+While comparing the variables, it first checks if the types differ. If they do, it returns false. 
+If the types match, then it checks for the value. If the values are same and are not numbers, it returns true
+*/
