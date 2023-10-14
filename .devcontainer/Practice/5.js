70cd29ce308d401c4b8ba0b4d1fc6ed638ce5159
Pragya Pant <pragyapant73@gmail.com>
2023-10-14 11:28:59 +0530

    done

---
+++ .devcontainer/Practice/5.js
@@ -0,0 +1,20 @@
+// Types of Data Types 
+
+// Primitive Data Types
+
+// number,String,Boolean,null,undefined,Bigint,symbol
+
+//Non-Primitive Data Types
+
+// object 
+/*  
+
+let person = { 
+               firstName: "Luiza", 
+               lastName: "Shaikh", 
+            }; 
+
+*/
+
+// Array
+  let d = new Array(1, 2, 3, "Hello"); 
